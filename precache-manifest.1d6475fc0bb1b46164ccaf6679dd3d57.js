22d51157617766cb7f304b15a2d5e8ef360b6b8a
JohnPCarvalho <johnnypcarvalho@gmail.com>
2020-07-09 14:13:24 -0300

    Updates

---
+++ precache-manifest.1d6475fc0bb1b46164ccaf6679dd3d57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0784bfa8d076ba2dad6720f7101702",
+    "url": "/GhibliReact/index.html"
+  },
+  {
+    "revision": "1e84f7a774d1f2fea896",
+    "url": "/GhibliReact/static/css/main.66ef133e.chunk.css"
+  },
+  {
+    "revision": "8f21c5fc0d0173bed707",
+    "url": "/GhibliReact/static/js/2.3eaacbcb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GhibliReact/static/js/2.3eaacbcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e84f7a774d1f2fea896",
+    "url": "/GhibliReact/static/js/main.3bc050fb.chunk.js"
+  },
+  {
+    "revision": "dfede651db66cea0b5ea",
+    "url": "/GhibliReact/static/js/runtime-main.0d2d18b1.js"
+  },
+  {
+    "revision": "e86d97c08805ce75a6e83bbd1c272455",
+    "url": "/GhibliReact/static/media/logo.e86d97c0.png"
+  }
+]);
